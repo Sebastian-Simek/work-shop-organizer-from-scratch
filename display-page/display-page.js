423f5c6a6213b591edb184aa7b58f09a9d190b93
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-24 16:47:59 -0700

    fixed error in display function

--- display-page/display-page.js
+++ display-page/display-page.js
@@ -1,23 +1,32 @@
 import { getWorkshops } from '../fetch-utils.js';
 
 const workshopsEl = document.getElementById('workshop-container');
-console.log(workshopsEl);
 
 async function displayWorkshops() {
     const workshops = await getWorkshops();
+    console.log(workshops);
     workshopsEl. textContent = '';
-    
     for (let workshop of workshops) {
         const div = document.createElement('div');
         div.textContent = workshop.name;
         div.classList.add('workshops');
+        console.log(workshop.participants);
+
+        const ul = document.createElement('ul');
+
+        for (let participant of workshop.participants) {
+            const li = document.createElement('li');
+            li.textContent = participant.name;
+
+            ul.append(li);
+            div.append(ul);
+        }
+        
+        
+        
         workshopsEl.append(div); 
     }
     
 }
 displayWorkshops();
 
-
-window.addEventListener('load', async () => {
-    await displayWorkshops();
-});
